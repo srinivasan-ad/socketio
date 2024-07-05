eb0b358518145ebb216a2aa50e925eb392be551b
srinivasan-ad <adityasrinivasan09@gmail.com>
2024-07-05 08:26:32 +0530

    added more ui

--- app/components/Mssgbox.js
+++ app/components/Mssgbox.js
@@ -1,22 +1,44 @@
-"use client";
-import React, { useState } from "react";
+'use client'
+import React, { useState,useEffect} from "react";
+
 
 function Mssgbox() {
   const [message, setMessage] = useState("");
   const [messagesList, setMessagesList] = useState([]);
 
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-  };
+  function handle_mssg_change(e) {
+    setMessage(e.target.value);
+  }
 
-  const handleMessageSend = () => {
+  function handle_mssg_sent() {
     if (message.trim() !== "") {
-      setMessagesList([...messagesList, message]);
+      const newMessage = { text: message, type: 'sent' };
+      setMessagesList([...messagesList, newMessage]);
       setMessage("");
     } else {
       alert("Message cannot be empty :)");
     }
+  }
+  function handle_enter(e){
+    if (e.key === "Enter") {
+      e.preventDefault(); 
+      handle_mssg_sent();
+    }
   };
+
+  function handle_mssg_recieved(receivedMessage) {
+    const newMessage = { text: receivedMessage, type: 'received' };
+    setMessagesList([...messagesList, newMessage]);
+  }
+
+  function simulateReceiveMessage() {
+    const receivedMessage = "This is a received message";
+    handle_mssg_recieved(receivedMessage);
+  }
+
+  useEffect(() => {
+    simulateReceiveMessage();
+  }, []);
 
   return (
     <div className="flex flex-col">
@@ -24,10 +46,15 @@ function Mssgbox() {
 
       <div className="w-full h-[100vh] flex flex-col justify-center items-center">
         <div className="h-[500px] w-[800px] border border-black rounded-md">
-          <div className="h-[460px] w-[800px] overflow-y-auto p-4 ">
+          <div className="h-[460px] w-[800px] overflow-y-auto p-4">
             {messagesList.map((msg, index) => (
-              <div key={index} className="mb-2 bg-slate-200">
-                {msg}
+              <div
+                key={index}
+                className={`mb-2 p-2 rounded-md ${
+                  msg.type === "received" ? "bg-blue-200" : "bg-gray-200"
+                }`}
+              >
+                {msg.text}
               </div>
             ))}
           </div>
@@ -37,11 +64,13 @@ function Mssgbox() {
               placeholder="Enter message...."
               className="border border-slate-400 w-[760px]"
               value={message}
-              onChange={handleMessageChange}
+              onChange={handle_mssg_change}
+              onKeyDown={handle_enter}
             />
             <button
+            type="submit"
               className="border border-gray-400 bg-slate-300 text-sm h-[26px] w-10"
-              onClick={handleMessageSend}
+              onClick={handle_mssg_sent}
             >
               Send
             </button>
